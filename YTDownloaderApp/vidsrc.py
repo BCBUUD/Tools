8c29668b6ea48a8719ed8cf604ae2878328029a5
BCBUUD <115499352+BCBUUD@users.noreply.github.com>
2022-10-17 10:52:16 -0700

    fix-ups
    Better functionality

--- YTDownloaderApp/vidsrc.py
+++ YTDownloaderApp/vidsrc.py
@@ -1,36 +1,47 @@
 import imdb
 import webbrowser
+import time 
+
 
 def movien():
     movie = input("\nMovie: ")
-    ia = imdb.IMDb() # imdb api 
-    results = ia.search_movie(movie) # searching...
+    MovieCrawler = imdb.IMDb() # imdb api 
+    results = MovieCrawler.search_movie(movie) # searching...
     mv = results[0] #First result
-    URL = ia.get_imdbURL(mv) #URL for first result
+    URL = MovieCrawler.get_imdbURL(mv) #URL for first result
     imdurl = URL
     vidurlf = imdurl.replace("https://www.imdb.com/title/","https://v2.vidsrc.me/embed/")
     x = vidurlf 
-    print("Playing in You Webbrowser...")
-    webbrowser.open(x)
-    print("\nMovie Link: ", x)
+    print("\n[L] Link \n[B] Browser")
+    option = input("\n")
+    if option == "l":
+        print("\nMovie Link: ", x)
+    if option == "b":
+        print("Playing in WebBrowser...")
+        webbrowser.open(x)
 
 
 
 def series():
     seris = input("\nSeries: ")
-    ia = imdb.IMDb()
-    results = ia.search_movie(seris)
+    MovieCrawler = imdb.IMDb()
+    results = MovieCrawler.search_movie(seris)
     mv = results[0] #First result
-    URL = ia.get_imdbURL(mv) #URL for first result
+    URL = MovieCrawler.get_imdbURL(mv) #URL for first result
     imdurl = URL
     seasonx = input("Season (in 01-15 format): ")
     episodex = input("Episode (in 01-15 format): ") 
     vidurlf = imdurl.replace("https://www.imdb.com/title/","https://v2.vidsrc.me/embed/")
     x = vidurlf + seasonx + "-" + episodex 
-    print("Playing in You Webbrowser...")
-    webbrowser.open(x)
+    print("\n[L] Link \n[B] Browser")
+    option = input("\n")
+    if option == "l":
+        print("\nShow Link: ", x)
+    if option == "b":
+        print("Playing in WebBrowser...")
+        webbrowser.open(x)
 
-print("\033[1;32m\n                Visit: https://isg32.github.io/Home                     \n")
+                
 
 print("\n[1] Movie \n[2] Series")
 decis = int(input("\n>> "))
@@ -42,16 +53,14 @@ if decis == 2:
 for i in range(30):
     print("(r)Reselect")
     print("(q)Quit")
-   
     rangeinput = input(">> ")
-    
-    
     while True:
         while True:
             answer = str(input('Anything else? (y/n): '))
             if answer in ('y', 'n'):
                 break
             print("invalid input.")
+
         if answer == 'y':
             print("\n[1] Movie \n[2] Series")
             decis = int(input("\n>> "))
