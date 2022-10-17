674d852292274bc171833f8231e992b9ad0e2123
BCBUUD <115499352+BCBUUD@users.noreply.github.com>
2022-10-17 07:55:32 -0700

    Update Docker-Builder.sh

--- BASH/Docker-Builder.sh
+++ BASH/Docker-Builder.sh
@@ -2,3 +2,15 @@
 
 # docker containers that build out python testing containers and pentesting suites within them.
 
+# docker with python enviornment 
+
+# docker with apache2 or nginx web server 
+
+# mysql or flask backend server for web server logic 
+
+# minecraft docker server 
+
+# livestream server connected to apache web server container 
+
+# mini nas server 
+
